48a70fefa9f77670a487ced31446a5dbb222c521
devlp121 <41315252+devlp121@users.noreply.github.com>
2021-02-04 07:54:07 +0300

    Update batch_inference.py

--- inference/batch_inference.py
+++ inference/batch_inference.py
@@ -1,8 +1,10 @@
 import os
 import glob
 import pandas as pd
+import json
 
 from infer import _infer_example
+
 
 
 cwd = os.getcwd()
@@ -11,24 +13,24 @@ filenames = glob.glob(cwd + "/*.csv")
 
 def _load_csv_files(file_path):
     df_list = []
+    print("Copying files: ",file_path)
     for filename in file_path:
-        df_list.merge(pd.read_csv(filename, names=["text"]))
+        df_list.append(pd.read_csv(filename, usecols=['text']))
 
     _combined_ds = pd.concat(df_list, ignore_index=True)
-    print("Copying files: ",df_list)
 
     return _combined_ds
 
 def _make_batched_inference(files):
     _series_txt = _load_csv_files(filenames)
-    _tensor_list = []
-    for row in _series_txt:
-        _tensor_list.append(_series_txt[row])
-    print(_tensor_list)
 
-    return _infer_example(_tensor_list)
+    _pred_list = []
 
-_make_batched_inference(filenames)
+    for value in _series_txt.values:
+        pred = _infer_example(str(value))
+        _pred_list.append(pred)
+    prediction = json.dumps(_pred_list, indent=4)
+    return prediction
 
-
-
+result = _make_batched_inference(filenames)
+print(result.predictions.mean())
